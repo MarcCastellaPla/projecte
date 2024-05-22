29c5a86931b73f63f3e8ee442e75d87fd1f1a9b2
MCP <marc.castellap@mataro.epiaedu.cat>
2024-05-22 20:34:16 +0200

    Primer commit

---
+++ firebase/js/config.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyArCKYqxtudHIznNGQU8FEM0r1v60tetwY",
+    authDomain: "items-c96fa.firebaseapp.com",
+    projectId: "items-c96fa",
+    storageBucket: "items-c96fa.appspot.com",
+    messagingSenderId: "508418114104",
+    appId: "1:508418114104:web:fd99543fee79a0c4a65ccd"
+});
+
+const auth = firebase.auth();
